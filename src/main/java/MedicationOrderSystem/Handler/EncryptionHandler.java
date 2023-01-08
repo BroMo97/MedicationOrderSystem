d4fa6f859b45740ede8e54da34b8e5bb8f897626
Patricia Bombik <patricia.bombik@stud.fra-uas.de>
2023-01-08 16:59:16 +0100

    UserHandler update

---
+++ src/main/java/MedicationOrderSystem/Handler/EncryptionHandler.java
@@ -0,0 +1,40 @@
+package MedicationOrderSystem.Handler;
+
+    import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * a class which helps to facilitate a certain extend of security regarding 
+ * {@link Classes.User} profiles. Passwords are encrypted
+ * @author Patricia
+ */
+public class EncryptionHandler {
+    
+    /**
+     * function to digest a password of a {@link Classes.User} and return it
+     * @param input the password of a {@link Classes.User}
+     * @return the encrypted password
+     */
+    public static String encryptPassword(String input, String username) {
+        try {
+            String new_input = new StringBuilder(input).append(username).toString();
+            //Encryption Object is built
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            //original password is parsed into the encrypter
+            messageDigest.update(new String(new_input).getBytes());
+            //password is encrypted
+            byte[] digestedBytes = messageDigest.digest();
+            //String builder since the encrypted password ain't a String anymore.
+            StringBuilder sb = new StringBuilder();
+            //Format the encrypted Password to its respective String Value to return it
+            for (byte b : digestedBytes) {
+                sb.append(String.format("%02x", b));
+            }
+
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+        return "";
+    }
+}
--- src/main/java/MedicationOrderSystem/Handler/UserHandler.java
+++ src/main/java/MedicationOrderSystem/Handler/UserHandler.java
@@ -1,5 +1,66 @@
 package MedicationOrderSystem.Handler;
+import Classes.Registration;
+import Classes.User;
 
+/**
+ * Handler to handle {@link User} when they're registered
+ * @author patricia
+ */
 public class UserHandler {
-    
+        
+    /**
+     * Checks wether Mail and Password are matching their confirmations
+     * respectively
+     * @param toBeConfirmed
+     * @return true if Mail and password are matching
+     */
+    public boolean confirmationCheck(Registration toBeConfirmed){
+        return toBeConfirmed.getEmail().equals(toBeConfirmed.getConfirm_email())    && 
+                toBeConfirmed.getPassword().equals(toBeConfirmed.getConfirm_password());
+        }
+        
+        /*
+        checking if the textfilds are null at the registration -> cant be null 
+        if null there comes a message like "please insert name" 
+        */  
+        /**
+         * Checks the input of a user when he or she registrates
+         * @param toBeChecked
+         * @return true, if the input is valid, else false
+         */
+        public boolean CheckInput (Registration toBeChecked)
+        {
+            System.out.println(toBeChecked.getPassword());
+            return (toBeChecked.getUsername().matches("^[a-zA-Z0-9_]{3,}$")
+                    && toBeChecked.getFirstName().matches("^[a-zA-Z]{3,}$")
+                    && toBeChecked.getLastName().matches("^[a-zA-Z]{3,}$")
+                    && toBeChecked.getPassword().matches("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$"));
+              
+        }
+        
+        /**
+         * Check if user input and the confirmation input was valid
+         * @param toBeCreated
+         * @return true, if valid, else false 
+         */
+        public boolean tryToCreateNewUser(Registration toBeCreated)
+        { 
+            return !(!CheckInput(toBeCreated) ||!confirmationCheck(toBeCreated));
+        }
+        
+        public Classes.User createNewUser(Registration toBeCreated,boolean EmailAvailable, boolean UsernameAvailable){
+            Classes.User createdUser = null;
+            
+            if(!EmailAvailable || !UsernameAvailable)
+                return createdUser;
+            
+            createdUser = 
+                    new User(toBeCreated.getFirstName(),
+                             toBeCreated.getLastName(),
+                             toBeCreated.getUsername(),
+                             toBeCreated.getEmail(), 
+                             false);
+            
+            return createdUser;
+        }
 }
