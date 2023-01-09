c79476a9cedb40d1d6dd3bdba3c90d8a25734570
Patricia Bombik <patricia.bombik@stud.fra-uas.de>
2023-01-09 21:29:14 +0100

    Order Class

--- src/main/java/MedicationOrderSystem/Classes/Orders.java
+++ src/main/java/MedicationOrderSystem/Classes/Orders.java
@@ -13,30 +13,20 @@ public class Orders {
     enum status {received, in_progess, ready}
 
     private int id = -1;
-    private String order_name = null;
+    private String medication = null;  //Medication
     private LocalDateTime order_date = null;
+    private String status = null; 
 
     //---------------------------- bin bis hier ---------------
-    private String location = null;
-    private java.util.ArrayList<User> participants = null;
-    //private String attachementfiles;
-    private String priority = null;
-    //private String reminder = null;
+    
+// constructor
 
-
-    public Orders(int id, String name, User host,
-    LocalDateTime dateTime, int duration,
-    String location, String priority, ArrayList<User> participants) {
-this.setId(id);
-this.setName(name);
-//this.setHost(host);
-this.setDateTime(dateTime);
-//this.setDuration(duration);
-this.setLocation(location);
-this.setParticipants(participants);
-this.setPriority(priority);
+    public Orders(int id, String mediaction, LocalDateTime order_date, String status ) {
+        this.setId(id);
+    
 
 }
+
 
 public Orders(String name, User host,
     LocalDateTime dateTime, int duration,
@@ -52,95 +42,49 @@ this.setPriority(priority);
 
 }
 
-public Orders(User user) {
-//setHost(user);
-setDateTime(LocalDateTime.now());
-setParticipants(user);
-}
-
 //methods 
 public void setId(int _id) {
 id = _id;
 }
 
-public int getID() {
+public int getId() {
 return id;
 }
 
-public String getName() {
-if (order_name != null) {
-    return order_name;
+public String getMedication() {
+if (medication != null) {
+    return medication;
 }
 return "";
 }
 
-public void setName(String _name) {
-order_name = _name;
+public void setMedication(String _medication) {
+medication = _medication;
 }
 
-//public User getHost() {
-//return host;
-//}
-//
-//public void setHost(User _host) {
-//host = _host;
-//}
 
-public LocalDateTime getDateTime() {
+
+public LocalDateTime getOrderDate() {
 if (order_date != null) {
     return this.order_date;
 }
 return LocalDateTime.MAX;
 }
 
-public void setDateTime(LocalDateTime _dateTime) {
-order_date = _dateTime;
-}
-
-//public int getDuration() {
-//return duration;
-//}
-//
-//public void setDuration(int _duration) {
-//duration = _duration;
-//}
-
-public String getLocation() {
-if (location != null) {
-    return location;
-}
-return "";
-}
-
-public void setLocation(String _location) {
-location = _location;
-}
-
-public java.util.ArrayList<User> getParticipants() {
-return participants;
-}
-
-public void setParticipants(java.util.ArrayList<User> _participants) {
-participants = _participants;
+public void setOrderDate(LocalDateTime _orderdate) {
+order_date = _orderdate;
 }
 
 
-public String getPriority() {
-return priority;
+public String getStatus() {
+return status;
 }
 
-public void setPriority(String _priority) {
-priority = _priority;
+public void setStatus(String _status) {
+status = _status;
 }
 
-
-//@Override
-//public String toString() {
-//return this.getName() + " "
-//        + this.getDateTime().toLocalDate().toString() + " "
-//        + this.getHost();
-//}
-
+/* 
 @Override
 public boolean equals(Object other) {
 
@@ -149,22 +93,10 @@ if (other == this) {
     return true;
 }
 
-/* Check if other is an instance of User or not 
-  "null instanceof [User]" also returns false */
+Check if other is an instance of User or not 
+  "null instanceof [User]" also returns false 
 if (!(other instanceof Orders)) {
     return false;
 }
-
-// Compare the usernames and return accordingly 
-return this.id == ((Orders) other).id;
-}
-
-private void setParticipants(User user) {
-if (this.participants == null) {
-    this.participants = new ArrayList<User>();
-}
-
-this.participants.add(user);
-
-}
+}*/
 }
